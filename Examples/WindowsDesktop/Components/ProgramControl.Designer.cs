99442e966c2cd08b1adee69d4c9b4f45aa159cf7
AlfredTouring <underautomation@gmail.com>
2021-10-12 17:43:16 +0200

    add program list

--- Examples/WindowsDesktop/Components/ProgramControl.Designer.cs
+++ Examples/WindowsDesktop/Components/ProgramControl.Designer.cs
@@ -33,6 +33,11 @@ partial class ProgramControl
             this.columnHeaderValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbPrograms = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,9 +49,9 @@ partial class ProgramControl
             this.columnHeaderValue});
             this.lstVariables.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstVariables.HideSelection = false;
-            this.lstVariables.Location = new System.Drawing.Point(0, 39);
+            this.lstVariables.Location = new System.Drawing.Point(0, 104);
             this.lstVariables.Name = "lstVariables";
-            this.lstVariables.Size = new System.Drawing.Size(660, 437);
+            this.lstVariables.Size = new System.Drawing.Size(660, 372);
             this.lstVariables.TabIndex = 0;
             this.lstVariables.UseCompatibleStateImageBehavior = false;
             this.lstVariables.View = System.Windows.Forms.View.Details;
@@ -68,22 +73,75 @@ partial class ProgramControl
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnRun);
+            this.panel1.Controls.Add(this.cbPrograms);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(660, 39);
+            this.panel1.Size = new System.Drawing.Size(660, 104);
             this.panel1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Location = new System.Drawing.Point(3, 88);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(140, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "List of all program variables :";
+            // 
+            // cbPrograms
+            // 
+            this.cbPrograms.FormattingEnabled = true;
+            this.cbPrograms.Location = new System.Drawing.Point(154, 8);
+            this.cbPrograms.Name = "cbPrograms";
+            this.cbPrograms.Size = new System.Drawing.Size(257, 21);
+            this.cbPrograms.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Programs saved in the robot :";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(417, 8);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(52, 21);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(475, 8);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(52, 21);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(533, 8);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(52, 21);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // ProgramControl
             // 
@@ -107,4 +165,9 @@ partial class ProgramControl
     private System.Windows.Forms.ColumnHeader columnHeaderValue;
     private System.Windows.Forms.Panel panel1;
     private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox cbPrograms;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button btnRun;
+    private System.Windows.Forms.Button btnStop;
+    private System.Windows.Forms.Button btnPause;
 }
--- Examples/WindowsDesktop/Components/ProgramControl.cs
+++ Examples/WindowsDesktop/Components/ProgramControl.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Windows.Forms;
 using UnderAutomation.UniversalRobots;
+using System.Linq;
+using System;
 
 public partial class ProgramControl : UserControl, IUserControl
 {
@@ -62,6 +64,43 @@ public partial class ProgramControl : UserControl, IUserControl
     }
 
     public void OnClose() { }
-    public void OnOpen() { }
+    public void OnOpen()
+    {
+        cbPrograms.Items.Clear();
+
+        if (!_ur.SftpEnabled || string.IsNullOrEmpty(_ur.IP)) return;
+
+        var loadedPrograms = _ur.GetLoadedProgram();
+        var splitedPath = loadedPrograms.Value.Split(new string[] { "programs/" }, System.StringSplitOptions.RemoveEmptyEntries);
+        var pathToPrograms = $"{splitedPath[0]}programs";
+
+        var items = _ur.SFTP.ListDirectory(pathToPrograms);
+
+        var programs = items.Where(x => x.Name.EndsWith(".urp", StringComparison.InvariantCultureIgnoreCase)).Select(x => x.Name).ToArray();
+
+        cbPrograms.Items.AddRange(programs);
+        if (splitedPath.Length > 0) cbPrograms.Text = splitedPath[1];
+    }
     #endregion
+
+    private void btnRun_Click(object sender, System.EventArgs e)
+    {
+        var result = _ur.LoadProgram(cbPrograms.Text);
+
+        if (!result.Succeed) throw new Exception(result.Message);
+
+        result = _ur.Play();
+
+        if (!result.Succeed) throw new Exception(result.Message);
+    }
+
+    private void btnPause_Click(object sender, EventArgs e)
+    {
+        _ur.Pause();
+    }
+
+    private void btnStop_Click(object sender, EventArgs e)
+    {
+        _ur.Stop();
+    }
 }
